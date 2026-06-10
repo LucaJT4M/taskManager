eb8c4227034be9b0124ffd0b08a0b2477dd3df2e
LucaJT4M <luca.jaculi@trans4mation.de>
2026-06-10 12:11:24 +0200

    adding espfiles

---
+++ espTaskManager/src/main.cpp
@@ -0,0 +1,201 @@
+#include <Adafruit_SH110X.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <vector>
+
+// configs für get requests
+const char* ssid = "MikroTik-AF2E92"; // man brauch ssid vom Netzwerk 
+String API_URL = "http://192.168.88.69:8000/get_tasks_for_esp"; // und die api url
+
+// Für SH1106 Displays nutzt man diesen Konstruktor:
+Adafruit_SH1106G display(128, 64, &Wire, -1);
+
+#define button_refresh 17
+#define button_two 15
+#define button_three 16
+
+
+void write_to_monitor(String text) {
+    display.clearDisplay();
+    display.setCursor(0, 10);
+    display.println(text);
+    display.display();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22); // Definiere deine I2C Pins explizit1
+  pinMode(button_refresh, INPUT_PULLUP);
+  pinMode(button_two, INPUT_PULLUP);
+  pinMode(button_three, INPUT_PULLUP);
+  
+  // SH1106 nutzt eine leicht andere Initialisierung:
+  if(!display.begin(0x3C, true)) { 
+    for(;;); 
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+
+  write_to_monitor("Begin");
+
+  // wifi setup
+  WiFi.begin(ssid, "");
+  Serial.println("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected");
+  
+  if (WiFi.status() == WL_CONNECTED) {
+    write_to_monitor("Connected");
+    HTTPClient http;
+    
+    http.begin(API_URL);  // Start connection
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      Serial.printf("HTTP Response Code: %d\n", httpCode);
+
+      String payload = http.getString();
+      Serial.println("Response:");
+      Serial.println(payload);
+    } else {
+      Serial.printf("Request failed: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+}
+
+class Task {
+  public:
+    int id;
+    String title;
+    bool checked;
+
+    Task parse_json(const String& json_string) {
+      Task task;
+      JsonDocument doc;
+      if (!deserializeJson(doc, json_string)) {
+        task.id = doc["id"] | 0;
+        task.title = doc["title"] | "";
+        task.checked = doc["checked"] | false; 
+      }
+
+      return task;
+    }
+};
+
+void parse_list_from_json(const String& json) {
+    std::vector<Task> tasks;
+    JsonDocument doc;
+
+    DeserializationError error = deserializeJson(doc, json);
+    if (error) {
+        Serial.print("JSON parse failed: ");
+        Serial.println(error.c_str());
+        return;
+    }
+
+    JsonArray array = doc.as<JsonArray>();
+
+    tasks.clear();
+
+    for (JsonObject obj : array) {
+        Task task;
+
+        task.id = obj["id"] | 0;
+        task.title = obj["title"] | "";
+        task.checked = obj["checked"] | false;
+
+        tasks.push_back(task);
+    }
+
+    Serial.printf("Loaded %d tasks\n", tasks.size());
+}
+
+class Button {
+  public:
+    int last_state = HIGH;
+    int state;
+    String _name;
+
+    Button(String name) {
+      _name = name;
+    }
+
+    bool Update() {
+      bool got_pressed = false;
+      if (last_state == LOW && state == HIGH) {
+        Serial.println(_name);
+        got_pressed = true;
+      } else {
+        got_pressed = false;
+      }
+      last_state = state;
+      return got_pressed;
+    }
+};
+
+Button refresh_btn("api refresh");
+Button btn_two("button two");
+Button btn_three("button three");
+
+void get_tasks_from_api() {
+  HTTPClient http;
+
+  http.begin(API_URL);  // Start connection
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    Serial.printf("HTTP Response Code: %d\n", httpCode);
+
+    String payload = http.getString();
+    Serial.println("Response:");
+    Serial.println(payload);
+    return payload;
+  } else {
+    Serial.printf("Request failed: %s\n",
+                  http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+}
+
+void print_task_ui(Task task) {
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.println("Id: " + task.id);
+  display.setCursor(1, 10);
+  display.println("Titel: " + task.title);
+  display.setCursor(2, 10);
+  if (task.checked) {
+    display.println("Erledigt: Ja");
+  } else {
+    display.println("Erledigt: Nein");
+  }
+  display.display();
+}
+
+std::vector<Task> tasks;
+
+void loop() {
+  // Put your main code here to run repeatedly
+  refresh_btn.state = digitalRead(button_refresh);
+  btn_two.state = digitalRead(button_two);
+  btn_three.state = digitalRead(button_three);
+
+  bool got_refreshed = refresh_btn.Update();
+
+  if (got_refreshed) {
+    get_tasks_from_api();
+  }
+  
+  btn_two.Update();
+  btn_three.Update();
+}
